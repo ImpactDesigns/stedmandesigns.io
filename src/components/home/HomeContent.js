4c9a4c4ca9acffaa31d52bb7da5fbb495f845451
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2022-03-09 21:22:41 -0600

    Updated home page

--- src/components/home/HomeContent.js
+++ src/components/home/HomeContent.js
@@ -1,10 +1,72 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledSection = styled.section`
+  padding: 0px 20px 0px 20px;
+  height: calc(100vh - 80px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  // border: 2px solid orange;
+`
+
+const TitleText = styled.h1`
+  margin: 0px;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 32px;
+  line-height: 40px;
+  text-align: center;
+  color: #586165;
+  // border: 1px dashed green;
+
+  @media (min-width: 768px) {
+    font-size: 40px;
+    line-height: 40px;  
+  }
+`
+
+const SubTitleText = styled.h2`
+  margin: 8px 0px 32px 0px;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 20px;
+  text-transform: uppercase;
+  text-align: center;
+  color: #586165;
+  // border: 1px dashed green;
+
+  @media (min-width: 768px) {
+    font-size: 24px;
+    line-height: 28px;  
+  }
+`
+
+const BodyText = styled.p`
+  margin: 0px;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 16px;
+  text-align: center;
+  color: #586165;
+  // border: 1px dashed green;
+
+  @media (min-width: 768px) {
+    font-size: 24px;
+    line-height: 28px;  
+  }
+`
 
 export default function HomeContent() {
   return (
-    <section>
-        <h1>Stedman Designs</h1>
-        <p>Content coming soon</p>
-    </section>
+    <StyledSection>
+        <div>
+          <TitleText>Stedman Designs</TitleText>
+          <SubTitleText>UX Designer & Fullstack Developer</SubTitleText>
+          <BodyText>Content is on it's way 😊</BodyText>
+        </div>
+    </StyledSection>
   )
 }
