3d3cc8990582cf0e32d71dbd99fe84c97fea3c54
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2022-03-10 13:02:08 -0600

    Added content for Hero section

--- src/components/home/HeroSection.js
+++ src/components/home/HeroSection.js
@@ -2,16 +2,26 @@ import React from 'react'
 import styled from 'styled-components'
 
 const StyledSection = styled.section`
-    padding: 0px 20px 20px 20px;
+    padding: 20px 20px 20px 20px;
     //   min-height: calc(100vh - 80px);
     display: grid;
     grid-template-columns: repeat(12, 1fr);
     grid-template-rows: auto;
     border: 2px solid orange;
+
+    @media (min-width: 768px) {
+        padding: 20px 80px 20px 80px;
+    }
+
+    @media (min-width: 1024px) {
+        padding-top: 80px;
+        padding-left: 144px;
+        padding-right: 144px;
+    }
 `
 
 const HeroTitle = styled.h1`
-    margin: 0px;
+    margin: 0px 0px 32px 0px;
     grid-column: 1 / 13;
     font-style: normal;
     font-weight: 700;
@@ -26,10 +36,15 @@ const HeroTitle = styled.h1`
         font-size: 48px;
         line-height: 56px;
     }
+
+    @media (min-width: 1024px) {
+        margin-bottom: 16px;
+        grid-column: 3 / 11;
+    }
 `
 
 const HeroSubtitle = styled.h2`
-    margin: 0px;
+    margin: 0px 0px 4px 0px;
     grid-column: 1 / 13;
     font-style: normal;
     font-weight: 600;
@@ -43,6 +58,10 @@ const HeroSubtitle = styled.h2`
         font-size: 32px;
         line-height: 40px;
     }
+
+    @media (min-width: 768px) {
+        grid-column: 3 / 11;
+    }
 `
 
 const HeroBody =  styled.h3`
@@ -54,7 +73,7 @@ const HeroBody =  styled.h3`
     line-height: 20px;
     text-align: center;
     color: #586165;
-    // border: 1px dashed green;
+    border: 1px dashed green;
 
     & > span {
         border-bottom: 1px solid #FFBD99;
@@ -62,6 +81,31 @@ const HeroBody =  styled.h3`
 
     & > span:nth-child(2) {
         border-bottom: 1px solid #CDCEFA;
+    }
+
+    @media (min-width: 1024px) {
+        margin-top: 16px;
+        grid-column: 3 / 11;
+        // font-size: 20px;
+        // line-height: 24px;
+    }
+`
+
+const HeroBody2 = styled.p`
+    margin: 56px 0px 0px 0px;
+    grid-column: 1 / 13;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 16px;
+    text-align: center;
+    text-transform: uppercase;
+    color: #586165;
+    border: 1px dashed green;
+
+    @media (min-width: 1024px) {
+        margin-top: 40px;
+        font-size: 16px;
     }
 `
 
@@ -71,6 +115,7 @@ export default function HeroSection() {
         <HeroTitle>Howdy! 👋</HeroTitle>
         <HeroSubtitle>Folks call me Stedman.</HeroSubtitle>
         <HeroBody>I find user centered solutions for digital experiences as a <span>UX Designer</span>. I make it real as a <span>Fullstack Developer</span>.</HeroBody>
+        <HeroBody2>Check out my projects below!</HeroBody2>
     </StyledSection>
   )
 }
