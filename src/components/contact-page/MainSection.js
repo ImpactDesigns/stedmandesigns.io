1c000eee0d17b4a4ab30b6a985960aeaf0c3f828
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2022-03-09 23:10:31 -0600

    Contact page styled for mobile

--- src/components/contact-page/MainSection.js
+++ src/components/contact-page/MainSection.js
@@ -3,12 +3,78 @@ import styled from 'styled-components'
 
 const StyledSection = styled.section`
   padding: 0px 20px 0px 20px;
-  height: calc(100vh - 80px);
-  border: 2px solid orange;
+  min-height: calc(100vh - 80px);
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  grid-template-rows: auto;
+//   border: 2px solid orange;
+`
+
+
+const Form = styled.form`
+    margin: 0px;
+    padding: 20px 0px 20px 0px;
+    grid-column: 1 / 13;
+    // border: 2px solid dodgerblue;
+`
+
+const Label = styled.label`
+    margin: 0px;
+    display: block;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 24px;
+    color: #586165;
+    // border: 1px dashed green;
+`
+
+const Textarea = styled.textarea`
+    box-sizing: border-box;
+    margin-top: 16px;
+    padding: 10px 12px 10px 12px;
+    width: 100%;
+    height: 240px;
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 28px;
+    color: #586165;
+    resize: none;
+    background: #fbfcfb;
+    border-radius: 0px;
+    border: 1px solid rgba(40, 42, 43, 0.2);
+    outline: none;
+`
+
+const StyledLabel = styled(Label)`
+    margin-top: 32px;
+    margin-bottom: 16px;
+    // border: 1px dashed green;
+`
+
+const StyledButton = styled.button`
+    margin-top: 20px;
 `
 
 export default function MainSection() {
-  return (
-    <StyledSection>Contact page</StyledSection>
-  )
+
+    function handleButtonClick(e) {
+        e.preventDefault()
+        alert('button has been fired')
+    }
+
+    return (
+        <StyledSection>
+            <Form>
+                <Label for={'messageTextArea'}>Hey Stedman,</Label>
+                <Textarea id={'messageTextArea'} name={'messageTextArea'} placeholder={'Write your message here...'} />
+                <StyledLabel>Regards,</StyledLabel>
+                <input placeholder='Your name' />
+                <input placeholder='Your email' className={'mt-12px'} />
+                <StyledButton onClick={(e) => handleButtonClick(e)}>Send email</StyledButton>
+            </Form>
+        </StyledSection>
+    )
 }
--- src/components/home/HomeContent.js
+++ src/components/home/HomeContent.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 
 const StyledSection = styled.section`
   padding: 0px 20px 0px 20px;
-  height: calc(100vh - 80px);
+  min-height: calc(100vh - 80px);
   display: flex;
   justify-content: center;
   align-items: center;
