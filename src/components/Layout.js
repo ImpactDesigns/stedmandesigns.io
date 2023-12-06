e9c51f23e9f75d29f94203fb41f11ab22da2727b
Stedman <michaelstedman81@gmail.com>
2023-12-06 12:36:41 -0600

    feat(Header): added Header to components index

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import AppContext from '../contexts/AppContext'
-import Header from './header/Header'
+import { Header } from '../components'
 
 export default function Layout({ children }) {
   return (
