b1419815963d6fe753ff12223b1d668b774bdb0c
Stedman <michaelstedman81@gmail.com>
2023-12-07 00:59:24 -0600

    feat: created FeaturedProjects component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,20 @@
-import React from "react"
-import Layout from '../components/Layout'
-import { HeroSection, PreviewSection } from '../components/pageComponents/home'
-import '../assets/index.css'
+import React from "react";
+import Layout from "../components/Layout";
+import {
+  HeroSection,
+  PreviewSection,
+  FeaturedProjectsSection,
+} from "../components/pageComponents/home";
+import "../assets/index.css";
 
 const IndexPage = () => {
   return (
     <Layout>
       <HeroSection />
+      <FeaturedProjectsSection />
       <PreviewSection />
     </Layout>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
